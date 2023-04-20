1e4970842a0fd19f010756030586df078ffd04d3
Alexandr <Ale906206@yandex.ru>
2023-04-21 00:33:20 +0300

    feat: реализованы бызовые CRUD - операции в FilmDbStorage и UserDbStorage

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmDbStorage.java
@@ -1,20 +1,21 @@
 package ru.yandex.practicum.filmorate.storage.film;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.stereotype.Repository;
+import ru.yandex.practicum.filmorate.exeption.NotFoundException;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.Genre;
 import ru.yandex.practicum.filmorate.model.Mpa;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.time.LocalDate;
 import java.util.*;
 
-
+@Slf4j
 @Repository
 @RequiredArgsConstructor
 public class FilmDbStorage implements FilmStorage {
@@ -43,19 +44,25 @@ public class FilmDbStorage implements FilmStorage {
     public Film update(Film film) {
         String sqlQuery = "UPDATE FILM SET NAME = ?, DESCRIPTION = ?, DURATION = ?, RELEASE_DATE = ?, MPA_ID = ? " +
                 "WHERE ID = ?";
-        jdbcTemplate.update(sqlQuery,
+        int rowsUpdated = jdbcTemplate.update(sqlQuery,
                 film.getName(),
                 film.getDescription(),
                 film.getDuration(),
                 film.getReleaseDate(),
                 film.getMpa().getId(),
-                film.getId());
+                film.getId()
+        );
+        if (rowsUpdated == 0) {
+            throw new NotFoundException("No such film in the database");
+        }
         return film;
     }
 
     @Override
     public List<Film> getAllFilms() {
-        String sqlQuery = "SELECT * FROM FILM";
+        String sqlQuery = "SELECT f.ID,f.NAME,f.DESCRIPTION,f.DURATION, f.RELEASE_DATE,f.MPA_ID,m.NAME AS MPA_NAME " +
+                "FROM FILM AS f " +
+                "JOIN MPA AS m ON f.MPA_ID = m.MPA_ID ";
         return jdbcTemplate.query(sqlQuery, this::mapRowToFilm);
     }
 
@@ -67,22 +74,25 @@ public class FilmDbStorage implements FilmStorage {
 
     @Override
     public Film get(int id) {
-        String sqlQuery = "SELECT * FROM FILM WHERE ID = ?";
+        String sqlQuery = "SELECT f.ID,f.NAME,f.DESCRIPTION,f.DURATION, f.RELEASE_DATE,f.MPA_ID,m.NAME AS MPA_NAME " +
+                "FROM FILM AS f " +
+                "JOIN MPA AS m ON f.MPA_ID = m.MPA_ID " +
+                "WHERE ID = ?";
         return jdbcTemplate.queryForObject(sqlQuery, this::mapRowToFilm, id);
     }
 
     private Film mapRowToFilm(ResultSet resultSet, int rowNum) throws SQLException {
-         Film film = Film.builder()
-                 .id(resultSet.getInt("ID"))
-                 .name(resultSet.getString("NAME"))
-                 .description(resultSet.getString("DESCRIPTION"))
-                 .duration(resultSet.getInt("DURATION"))
-                 .releaseDate((LocalDate) resultSet.getObject("RELEASE_DATE"))
-                 .mpa(getMpaByFilmId(resultSet.getInt("ID")))
-                 .build();
-        film.getGenres().addAll(getGenresByFilmId(film.getId()));
-        film.getLikes().addAll(getLikesByFilmId(film.getId()));
-        return film;
+            Film film = Film.builder()
+                    .id(resultSet.getInt("ID"))
+                    .name(resultSet.getString("NAME"))
+                    .description(resultSet.getString("DESCRIPTION"))
+                    .duration(resultSet.getInt("DURATION"))
+                    .releaseDate(resultSet.getDate("RELEASE_DATE").toLocalDate())
+                    .mpa(new Mpa(resultSet.getInt("MPA_ID"), resultSet.getString("MPA_NAME")))
+                    .build();
+            film.getGenres().addAll(getGenresByFilmId(film.getId()));
+            film.getLikes().addAll(getLikesByFilmId(film.getId()));
+            return film;
     }
 
     private Map<String, Object> getFilmFields(Film film) {
@@ -133,10 +143,5 @@ public class FilmDbStorage implements FilmStorage {
             res.add(rowSet.getInt("USER_ID"));
         }
         return res;
-    }
-    private Mpa getMpaByFilmId(int id) {
-        String sql = "SELECT * FROM MPA WHERE MPA_ID IN (SELECT MPA_ID FROM FILM WHERE FILM_ID = ?)";
-        return jdbcTemplate.queryForObject(sql, (rs, rowNum) ->
-                new Mpa(rs.getInt("MPA_ID"), rs.getString("NAME")));
     }
 }
--- src/main/java/ru/yandex/practicum/filmorate/storage/user/UserDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/user/UserDbStorage.java
@@ -2,10 +2,15 @@ package ru.yandex.practicum.filmorate.storage.user;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.stereotype.Repository;
+import ru.yandex.practicum.filmorate.exeption.NotFoundException;
 import ru.yandex.practicum.filmorate.model.User;
 
-import java.util.List;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.*;
 
 @Repository
 @RequiredArgsConstructor
@@ -15,26 +20,87 @@ public class UserDbStorage implements UserStorage {
 
     @Override
     public User add(User user) {
-        return null;
+        SimpleJdbcInsert simpleJdbcInsert = new SimpleJdbcInsert(jdbcTemplate);
+        Number key = simpleJdbcInsert.withTableName("USERS")
+                .usingGeneratedKeyColumns("ID")
+                .executeAndReturnKey(getUserFields(user));
+        if (!user.getFriends().isEmpty()) {
+            simpleJdbcInsert.withTableName("FRIENDS")
+                    .execute(getUserFriends(user));
+        }
+        user.setId(key.intValue());
+        return user;
     }
 
     @Override
     public User update(User user) {
-        return null;
+        String sql = "UPDATE USERS SET LOGIN = ?, NAME = ?, EMAIL = ?, BIRTHDAY = ? WHERE ID = ?";
+        int rowsUpdated = jdbcTemplate.update(sql,
+                user.getLogin(),
+                user.getName(),
+                user.getEmail(),
+                user.getBirthday(),
+                user.getId()
+        );
+        if (rowsUpdated == 0) {
+            throw new NotFoundException("No such user in the database");
+        }
+        return user;
     }
 
     @Override
     public List<User> getAllUsers() {
-        return null;
+        String sql = "SELECT * FROM USERS";
+        return jdbcTemplate.query(sql, this::mapRowToUser);
     }
 
     @Override
     public boolean delete(int id) {
-        return false;
+        String sql = "DELETE FROM USERS WHERE ID = ?";
+        return jdbcTemplate.update(sql, id) > 0;
     }
 
     @Override
     public User get(int id) {
-        return null;
+        String sql = "SELECT * FROM USERS WHERE ID = ?";
+        return jdbcTemplate.queryForObject(sql, this::mapRowToUser, id);
+    }
+
+    private Map<String, Object> getUserFields(User user) {
+        Map<String, Object> fields = new HashMap<>();
+        fields.put("LOGIN", user.getLogin());
+        fields.put("NAME", user.getName());
+        fields.put("EMAIL", user.getEmail());
+        fields.put("BIRTHDAY", user.getBirthday());
+        return fields;
+    }
+
+    private Map<String, Object> getUserFriends(User user) {
+        Map<String, Object> friends = new HashMap<>();
+        user.getFriends().forEach(
+                friend -> friends.put(String.valueOf(user.getId()), friend)
+        );
+        return friends;
+    }
+
+    private User mapRowToUser(ResultSet resultSet, int rowNum) throws SQLException {
+        User user = User.builder()
+                .id(resultSet.getInt("ID"))
+                .login(resultSet.getString("LOGIN"))
+                .name(resultSet.getString("NAME"))
+                .email(resultSet.getString("EMAIL"))
+                .birthday(resultSet.getDate("BIRTHDAY").toLocalDate())
+                .build();
+        user.getFriends().addAll(getFriendsByUserId(resultSet.getInt("ID")));
+        return user;
+    }
+    private Set<Integer> getFriendsByUserId(int id) {
+        String sql = "SELECT FRIEND_ID FROM FRIENDS WHERE USER_ID = ?";
+        SqlRowSet rowSet = jdbcTemplate.queryForRowSet(sql, id);
+        Set<Integer> res = new HashSet<>();
+        while(rowSet.next()) {
+            res.add(rowSet.getInt("FRIEND_ID"));
+        }
+        return res;
     }
 }
